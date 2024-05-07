ac7d248d2c59c0dde61474ed771bd5f55c72623e
Martin Dimitrov <martin.p.dimitrov@intel.com>
2024-05-07 14:51:40 -0700

    changes to __init__.py

--- packages/opentelemetry-instrumentation-redis/opentelemetry/instrumentation/redis/__init__.py
+++ packages/opentelemetry-instrumentation-redis/opentelemetry/instrumentation/redis/__init__.py
@@ -1,4 +1,4 @@
-"""OpenTelemetry Pinecone instrumentation"""
+"""OpenTelemetry Redis instrumentation"""
 
 import logging
 from opentelemetry.instrumentation.redis.config import Config
@@ -87,65 +87,65 @@ def _set_query_input_attributes(span, kwargs):
     # sparse_vector: Optional[Union[SparseValues, Dict[str, Union[List[float], List[int]]]]] = None,
     # **kwargs) -> QueryResponse:
 
-    _set_span_attribute(span, "pinecone.query.id", kwargs.get("id"))
-    _set_span_attribute(span, "pinecone.query.queries", kwargs.get("queries"))
-    _set_span_attribute(span, "pinecone.query.top_k", kwargs.get("top_k"))
-    _set_span_attribute(span, "pinecone.query.namespace", kwargs.get("namespace"))
-    _set_span_attribute(span, "pinecone.query.filter", kwargs.get("filter"))
-    _set_span_attribute(
-        span, "pinecone.query.include_values", kwargs.get("include_values")
-    )
-    _set_span_attribute(
-        span, "pinecone.query.include_metadata", kwargs.get("include_metadata")
-    )
+    # _set_span_attribute(span, "pinecone.query.id", kwargs.get("id"))
+    # _set_span_attribute(span, "pinecone.query.queries", kwargs.get("queries"))
+    # _set_span_attribute(span, "pinecone.query.top_k", kwargs.get("top_k"))
+    # _set_span_attribute(span, "pinecone.query.namespace", kwargs.get("namespace"))
+    # _set_span_attribute(span, "pinecone.query.filter", kwargs.get("filter"))
+    # _set_span_attribute(
+    #     span, "pinecone.query.include_values", kwargs.get("include_values")
+    # )
+    # _set_span_attribute(
+    #     span, "pinecone.query.include_metadata", kwargs.get("include_metadata")
+    # )
 
     # Log query embeddings
     # We assume user will pass either vector, sparse_vector or queries
     # But not two or more simultaneously
     # When defining conflicting sources of embeddings, the trace result is undefined
 
-    vector = kwargs.get("vector")
-    if vector:
-        span.add_event(
-            name="db.query.embeddings",
-            attributes={"db.query.embeddings.vector": vector},
-        )
+    # vector = kwargs.get("vector")
+    # if vector:
+    #     span.add_event(
+    #         name="db.query.embeddings",
+    #         attributes={"db.query.embeddings.vector": vector},
+    #     )
 
-    sparse_vector = kwargs.get("sparse_vector")
-    if sparse_vector:
-        span.add_event(
-            name="db.query.embeddings",
-            attributes={"db.query.embeddings.vector": sparse_vector},
-        )
+    # sparse_vector = kwargs.get("sparse_vector")
+    # if sparse_vector:
+    #     span.add_event(
+    #         name="db.query.embeddings",
+    #         attributes={"db.query.embeddings.vector": sparse_vector},
+    #     )
 
-    queries = kwargs.get("queries")
-    if queries:
-        for vector in queries:
-            span.add_event(
-                name=Events.DB_QUERY_EMBEDDINGS.value,
-                attributes={EventAttributes.DB_QUERY_EMBEDDINGS_VECTOR.value: vector},
-            )
-
+    # queries = kwargs.get("queries")
+    # if queries:
+    #     for vector in queries:
+    #         span.add_event(
+    #             name=Events.DB_QUERY_EMBEDDINGS.value,
+    #             attributes={EventAttributes.DB_QUERY_EMBEDDINGS_VECTOR.value: vector},
+    #         )
+    pass
 
 @dont_throw
 def _set_query_response(span, response):
-    span.add_event("pinecone.query.usage", response.get("usage"))
+    # span.add_event("pinecone.query.usage", response.get("usage"))
 
-    matches = response.get("matches")
+    # matches = response.get("matches")
 
-    for match in matches:
-        span.add_event(
-            name=Events.DB_QUERY_RESULT.value,
-            attributes={
-                EventAttributes.DB_QUERY_RESULT_ID.value: match.get("id"),
-                EventAttributes.DB_QUERY_RESULT_SCORE.value: match.get("score"),
-                EventAttributes.DB_QUERY_RESULT_METADATA.value: str(
-                    match.get("metadata")
-                ),
-                EventAttributes.DB_QUERY_RESULT_VECTOR.value: match.get("values"),
-            },
-        )
-
+    # for match in matches:
+    #     span.add_event(
+    #         name=Events.DB_QUERY_RESULT.value,
+    #         attributes={
+    #             EventAttributes.DB_QUERY_RESULT_ID.value: match.get("id"),
+    #             EventAttributes.DB_QUERY_RESULT_SCORE.value: match.get("score"),
+    #             EventAttributes.DB_QUERY_RESULT_METADATA.value: str(
+    #                 match.get("metadata")
+    #             ),
+    #             EventAttributes.DB_QUERY_RESULT_VECTOR.value: match.get("values"),
+    #         },
+    #     )
+    pass
 
 def _set_input_attributes(span, kwargs):
     pass
@@ -178,7 +178,7 @@ def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
         name,
         kind=SpanKind.CLIENT,
         attributes={
-            SpanAttributes.VECTOR_DB_VENDOR: "Pinecone",
+            SpanAttributes.VECTOR_DB_VENDOR: "Redis",
         },
     ) as span:
         if span.is_recording():
