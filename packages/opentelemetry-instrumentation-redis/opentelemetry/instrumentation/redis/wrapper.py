30f4f206f94c66011b2b0eadfa559566e9e55960
Martin Dimitrov <martin.p.dimitrov@intel.com>
2024-05-16 22:11:10 -0700

    use args to extract the Query object from the search method and attach it to span attributes

--- packages/opentelemetry-instrumentation-redis/opentelemetry/instrumentation/redis/wrapper.py
+++ packages/opentelemetry-instrumentation-redis/opentelemetry/instrumentation/redis/wrapper.py
@@ -4,6 +4,7 @@ from opentelemetry.instrumentation.utils import (
 )
 #from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry import context as context_api
+from redis.commands.search.query import Query
 
 
 def _set_span_attribute(span, name, value):
@@ -15,30 +16,27 @@ def _set_span_attribute(span, name, value):
 @dont_throw
 def _set_generic_span_attributes(span):
     _set_span_attribute(span, "redis.generic", 1)
-    
+
+# Assume that the first argument is the Query object and we extract the query string  
 @dont_throw
-def _set_search_attributes(span, kwargs):
-    #print("kwargs in _set_search_attributes:", kwargs)
-    #print("kwargs.keys in _set_search_attributes:", kwargs.keys())
+def _set_search_attributes(span, args):
     _set_span_attribute(
         span,
         "redis.commands.search.query",
-        kwargs.get("query"),
+        args[0].query_string(),
     )
 
 @dont_throw
 def _set_create_index_attributes(span, kwargs):
-    #print("kwargs in _set_create_index_attributes:", kwargs)
-    #print("kwargs.keys in _set_create_index_attributes:", kwargs.keys())
     _set_span_attribute(
         span,
         "create_index",
-        kwargs.get("fields"),
+        kwargs.get("fields").__str__(),
     )
     _set_span_attribute(
         span,
         "create_index",
-        kwargs.get("definition"),
+        kwargs.get("definition").__str__(),
     )
     
 # @dont_throw
@@ -61,30 +59,24 @@ def _with_tracer_wrapper(func):
 def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
     """Instruments and calls every function defined in TO_WRAP."""
     
-    print("kwargs in _wrap:", kwargs)
-    print("kwargs.keys in _wrap:", kwargs.keys())
-    
     if context_api.get_value(_SUPPRESS_INSTRUMENTATION_KEY):
         return wrapped(*args, **kwargs)
 
     name = to_wrap.get("span_name")
     print("span name: ", name)
     with tracer.start_as_current_span(name) as span:  
-        #span.set_attribute(SpanAttributes.DB_SYSTEM, "redis")
-        #span.set_attribute(SpanAttributes.DB_OPERATION, to_wrap.get("method"))
+
         print("method: ", to_wrap.get("method"))
         if to_wrap.get("method") == "search":
-            #_set_search_attributes(span, kwargs)
-            _set_generic_span_attributes(span)
+            _set_search_attributes(span, args)
         elif to_wrap.get("method") == "create_index":
-            #_set_create_index_attributes(span, kwargs)
-            _set_generic_span_attributes(span)
+            _set_create_index_attributes(span, kwargs)
         else:
             _set_generic_span_attributes(span)
             
         response = wrapped(*args, **kwargs)
         
-        print("response", response)
+        #print("response", response)
         # if to_wrap.get("method") == "search":
         #     _add_search_result_events(span, response)
         
